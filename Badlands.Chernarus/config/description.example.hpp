4186480309058bc20014e3ce5e42c2090302fd1a
David Stewart <djranger1@gmail.com>
2014-04-20 19:15:37 -0700

    Move server specific description.ext into new file

---
+++ Badlands.Chernarus/config/description.example.hpp
@@ -0,0 +1,11 @@
+respawnDelay = 0;
+enableDebugConsole = 2;
+allowFunctionsRecompile = 1;
+allowFunctionsLog = 1;
+disableChannels[] = {2,6};
+
+class Header {
+	gameType = Sandbox; // Game type, see 'class' columns in the table below
+	minPlayers = 1; //min # of players the mission supports
+	maxPlayers = 70; //max # of players the mission supports
+};
