a65d437bea6eeeab8fd387835ad7118fb483afc1
David Stewart <djranger1@gmail.com>
2014-05-07 15:40:23 -0700

    Ignore loadScreen

--- Badlands.Chernarus/config/description.example.hpp
+++ Badlands.Chernarus/config/description.example.hpp
@@ -3,6 +3,7 @@ enableDebugConsole = 2;
 allowFunctionsRecompile = 1;
 allowFunctionsLog = 1;
 disableChannels[] = {2,6};
+loadScreen = "loadScreen.paa";
 
 class Header {
 	gameType = Sandbox; // Game type, see 'class' columns in the table below
