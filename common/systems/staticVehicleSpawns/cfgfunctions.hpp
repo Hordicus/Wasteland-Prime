b650260426dbfeca3fbb1c0ff61178ae70df3498
David Stewart <djranger1@gmail.com>
2014-07-02 15:34:22 -0700

    Fix staticVehicleSpawns path

--- common/systems/staticVehicleSpawns/cfgfunctions.hpp
+++ common/systems/staticVehicleSpawns/cfgfunctions.hpp
@@ -1,13 +1,13 @@
 class staticVehicleSpawns {
 	class staticVehicleSpawnsInit {
-		file = "\x\bl_server\addons\systems\staticVehicleSpawns\init.sqf";
+		file = "\x\bl_common\addons\systems\staticVehicleSpawns\init.sqf";
 		preInit = 1;
 	};
 	
 	class staticVehicleSpawnsConfig {
-		file = "\x\bl_server\addons\config\staticVehicleSpawns.sqf";
+		file = "\x\bl_common\addons\config\staticVehicleSpawns.sqf";
 	};
 
-	file = "\x\bl_server\addons\systems\staticVehicleSpawns\functions";
+	file = "\x\bl_common\addons\systems\staticVehicleSpawns\functions";
 	class staticVehKilled{};
 };
